578d17e50e061ef6c92a1e84795f001a3ffd09e1
Aslam-Akbar <aslam21tech@gmail.com>
2025-11-23 21:54:45 +0530

    vercel update 954

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    optimizePackageImports: [],
+    forceSwcTransforms: true,
   },
+  output: "standalone",
   reactStrictMode: true,
+  trailingSlash: false,
 };
 
 module.exports = nextConfig;
